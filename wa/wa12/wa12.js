055b5b7cad335110d1877e80c170ab758e757f40
Joseph-Meikle <70340493+Joseph-Meikle@users.noreply.github.com>
2025-07-31 21:24:21 -0600

    finally found an api that WORKS
    (mostly)

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,20 +1,29 @@
-const endpoint = "https://random-d.uk/api/v2/quack";
+const endpoint = "https://random.dog/woof.json";
 const picPlace = document.querySelector("body article img");
 const dogButton = document.querySelector("button");
-dogButton.addEventListener("click",getFact);
+dogButton.addEventListener("click",getDogPic);
+picPlace.setAttribute("height",String(0.6*window.innerHeight)+"px");
 
-async function getFact() {
+async function getDogPic() {
     try {
         const response = await fetch(endpoint);
         if (!response.ok){
             throw Error(response.statusText);
         }
-        const bearSrc = await response.text();
-        picPlace.setAttribute("src",bearSrc);
+        const dogObj = await response.json();
+        const dogUrl = await dogObj.url;
+        //console.log(dogUrl);
+        const fileEnding = dogUrl.substring(dogUrl.length-4);
+        if (fileEnding==".mp4" || fileEnding=="webm"){
+            console.log("This \"random dog picture\" API just gave me a VIDEO");
+            getDogPic();
+            return;
+        }
+        picPlace.setAttribute("src",dogUrl);
     } catch (err) {
         console.log(err);
-        alert("Bearless...");
+        alert("NO DOGS");
     }
 }
 
-getFact();//call once on page load
+getDogPic();//call once on page load
